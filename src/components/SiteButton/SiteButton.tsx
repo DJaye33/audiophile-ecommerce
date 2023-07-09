b6989986f8ffc9ff1f10cdd08d62e51b743f132d
Danny JeanLouis <dlouis3386@gmail.com>
2023-07-09 16:12:50 -0400

    make site button dynamic

--- src/components/SiteButton/SiteButton.tsx
+++ src/components/SiteButton/SiteButton.tsx
@@ -3,11 +3,25 @@ type SiteButtonTextProps = {
 };
 
 const SiteButton = ({ text }: SiteButtonTextProps) => {
-  return (
-    <button className="bg-amber-600 text-white py-4 px-9 hover:bg-amber-500">
-      {text.toUpperCase()}
-    </button>
-  );
+  if (text === "see product") {
+    return (
+      <a
+        onClick={() => console.log(text)}
+        className="bg-amber-600 text-white py-4 px-9 hover:bg-amber-500 inline-block cursor-pointer"
+      >
+        {text.toUpperCase()}
+      </a>
+    );
+  } else {
+    return (
+      <button
+        onClick={() => console.log(text)}
+        className="bg-amber-600 text-white py-4 px-9 hover:bg-amber-500"
+      >
+        {text.toUpperCase()}
+      </button>
+    );
+  }
 };
 
 export default SiteButton;
