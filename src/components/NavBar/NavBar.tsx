1b3a09aea81154e9a64ca1f68543bb91aa68cbd9
Danny JeanLouis <dlouis3386@gmail.com>
2023-07-09 11:36:50 -0400

    build navbar component

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,5 +1,19 @@
+import hamburgerMenu from "../../assets/shared/tablet/icon-hamburger.svg";
+import brandLogo from "../../assets/shared/desktop/logo.svg";
+
 const NavBar = () => {
-  return <div>Hello from NavBar</div>;
+  return (
+    <nav className="bg-neutral-950 py-8 px-6">
+      <div className="flex justify-between items-center">
+        <img src={hamburgerMenu} alt="menu" />
+        {/* TODO: Convert to react router Link tag */}
+        <a href="/">
+          <img src={brandLogo} alt="go home" />
+        </a>
+        <img src="/icon-cart.svg" alt="shopping cart" />
+      </div>
+    </nav>
+  );
 };
 
 export default NavBar;
