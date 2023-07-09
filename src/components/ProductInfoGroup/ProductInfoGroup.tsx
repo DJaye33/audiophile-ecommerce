ecc62331e1a56a2ccb49f5760a2290ee400d4bd0
Danny JeanLouis <dlouis3386@gmail.com>
2023-07-09 14:15:01 -0400

    build product info group

---
+++ src/components/ProductInfoGroup/ProductInfoGroup.tsx
@@ -0,0 +1,20 @@
+const ProductInfoGroup = () => {
+  return (
+    <div>
+      <div className="text-center">
+        <p className="text-gray-500 uppercase tracking-[10px] mb-4">
+          New Product
+        </p>
+        <h2 className="text-white text-4xl font-bold uppercase mb-6">
+          XX99 MARK II Headphones
+        </h2>
+        <p className="text-gray-300 font-bold mb-7">
+          Experience natural, lifelike audio and exceptional build quality made
+          for the passionate music enthusiast.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default ProductInfoGroup;
